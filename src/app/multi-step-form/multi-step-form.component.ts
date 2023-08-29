ae57cbdab281f08e9db0985ee744eb90688727ab
thekingvice <villarrealvicente92@gmail.com>
2023-08-28 18:27:11 -0700

    adding data and functions

--- src/app/multi-step-form/multi-step-form.component.ts
+++ src/app/multi-step-form/multi-step-form.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-multi-step-form',
   templateUrl: './multi-step-form.component.html',
-  styleUrls: ['./multi-step-form.component.css']
+  styleUrls: ['./multi-step-form.component.css'],
 })
 export class MultiStepFormComponent {
+  isAnnual = false;
 
+  prices = {
+    arcade: { monthly: 9, annually: 90 },
+    advanced: { monthly: 12, annually: 120 },
+    pro: { monthly: 15, annually: 150 },
+    onlineService: { monthly: 1, annually: 10 },
+    largerStorage: { monthly: 2, annually: 20 },
+    customizableProfile: { monthly: 2, annually: 20 },
+  };
+
+  setIsAnnual(event: Event) {
+    event.preventDefault();
+    this.isAnnual = !this.isAnnual;
+  }
 }
